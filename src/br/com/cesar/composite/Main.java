25288f4bddc0683f67986a2e31692bf2e0c4518a
Zarathon Maia <zarathonmaia@gmail.com>
2015-11-08 17:11:38 -0300

    Exercicio Composite Finalizado

--- src/br/com/cesar/composite/Main.java
+++ src/br/com/cesar/composite/Main.java
@@ -5,7 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Picture p = new Picture();
+		
+		Graphic g1 = new Line();
+		Graphic g2 = new Rectangle();
+		Graphic g3 = new Text();
+		
+		p.add(g1);
+		p.add(g2);
+		p.add(g3);
+		
 		p.draw();
+		
+		
 		
 
 	}
